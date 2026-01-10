91c963d3c02b397a88a6002db6e6b520af2b6dd5
abhishekmali09 <abhishekmali7710@gmail.com>
2026-01-10 17:35:35 +0530

    failed

--- src/main/java/com/BankingApplication/BankingApplication.java
+++ src/main/java/com/BankingApplication/BankingApplication.java
@@ -1,13 +1,5 @@
 package com.BankingApplication;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class BankingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BankingApplication.class, args);
-	}
-
+    
 }
